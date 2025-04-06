8b7d944f2cc53897914eece3accddbca7e572910
Matthew-Hughes3488 <12hughesm@gmail.com>
2025-04-06 14:54:32 +0100

    Updated title name

--- frontend/src/containers/Login/Login.tsx
+++ frontend/src/containers/Login/Login.tsx
@@ -10,7 +10,7 @@ export const Login = ({authFunction} : LoginProps) => {
 
   return (
     <div className="login-page">
-      <h1>Welcome to MusicPlayer</h1>
+      <h1>MusicPlayer</h1>
       <p>To get started, connect your Spotify account:</p>
       <Button buttonName="Connect to Spotify" buttonType="Test" onClick={authFunction}/>
     </div>
