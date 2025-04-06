6a27dc4d448d48c9c189171b3fa013c2dd07285f
Matthew-Hughes3488 <12hughesm@gmail.com>
2025-04-06 14:07:04 +0100

    Added login logic to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Main} from './containers/Main/Main';
+import { Login } from './containers/Login/Login';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false)
+  
   return (
     <div className="App">
-      <Main/>
+      {isLoggedIn ? <Main/> : <Login/>}
     </div>
   );
 }
