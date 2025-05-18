01d252d2c704bf044e716c4d474fa0cbcf6628a2
Matthew <12hughesm@gmail.com>
2025-05-18 20:07:23 +0100

    Updated router with http error codes

--- backend/user-service/routers/user_router.py
+++ backend/user-service/routers/user_router.py
@@ -4,6 +4,7 @@ from models.user_model import UserModel
 from models.user_input_model import UserInputModel
 from services.mock_user_service import MockUserService
 from repos.mock_user_repo import MockUserRepository
+from errors_exceptions.exceptions import UserNotFoundError
 
 
 router = APIRouter()
@@ -22,13 +23,28 @@ def get_user_by_id(user_id: int):
 
 @router.post("/users", response_model=UserModel)
 def create_user(user_input: UserInputModel):
-    return user_service.create_new_user(user_input)
+    try:
+        return user_service.create_new_user(user_input)
+    except ValueError as e:
+        raise HTTPException(status_code=409, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @router.put("/users/{user_id}", response_model=UserModel)
 def update_user(user_id: int, user_input: UserInputModel):
-    return user_service.update_user(user_id, user_input)
+    try:
+        return user_service.update_user(user_id, user_input)
+    except ValueError as e:
+        raise HTTPException(status_code=409, detail=str(e))
+    except UserNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 @router.delete("/users/{user_id}")
 def delete_user(user_id: int):
-    user_service.remove_user(user_id)
-    return {"detail": "User deleted"}
+    try:
+        user_service.remove_user(user_id)
+        return {"detail": "User deleted"}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
