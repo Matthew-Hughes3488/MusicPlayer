d5cbd7f22ec90aa623f1c73bf2ad3da67dcb4c05
Matthew <12hughesm@gmail.com>
2025-07-05 18:03:29 +0100

    created song service dirs and file by copying album serive

---
+++ backend/song-service/errors_exceptions/exceptions.py
@@ -0,0 +1,11 @@
+class AlbumNotFoundError(Exception):
+    """Raised when a user cannot be found in the system."""
+    pass
+
+class InvalidCredentialsError(Exception):
+    """Raised when login credentials are invalid."""
+    pass
+
+class PermissionDeniedError(Exception):
+    """Raised when a user attempts an unauthorized action."""
+    pass
